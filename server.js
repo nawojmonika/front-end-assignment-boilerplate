589cfc0a6e59e83f005fd9b1465fd63cb5a70e13
nawojmonika <nawojmonika@gmail.com>
2020-09-20 16:50:08 +0200

    Added uploading files to local directory

--- server.js
+++ server.js
@@ -2,20 +2,60 @@ const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const opn = require('opn');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
 
 const app = express();
 const config = require('./webpack.config.js');
 const compiler = webpack(config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
 app.use(
   webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath,
   }),
 );
 
-// Serve the files on port 3000.
+app.use(
+  fileUpload({
+    createParentPath: true,
+  }),
+);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.post('/upload-image', async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: 'No file uploaded',
+      });
+    } else {
+      let image = req.files.image;
+
+      image.mv('./public/images/' + image.name);
+
+      res.send({
+        status: true,
+        message: 'File is uploaded',
+        data: {
+          name: image.name,
+          mimetype: image.mimetype,
+          size: image.size,
+        },
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!\n');
   opn('http://localhost:3000/');
