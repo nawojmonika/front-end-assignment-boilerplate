0dce29a7259d6ae615c25621ef6969158a29ece3
nawojmonika <nawojmonika@gmail.com>
2020-09-20 13:07:16 +0200

    Added react to the project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // CSS imported here will be bundled by webpack
 import './index.css';
 // You can import any components here
-import './components/my-component';
+import './components/my-component/my-component';
