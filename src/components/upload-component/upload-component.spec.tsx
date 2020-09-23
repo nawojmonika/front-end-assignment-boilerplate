90bf2c43539a9db75f2ef6bd3743f2fa52fde7db
nawojmonika <nawojmonika@gmail.com>
2020-09-23 17:47:21 +0200

    Added another upload-component test

--- src/components/upload-component/upload-component.spec.tsx
+++ src/components/upload-component/upload-component.spec.tsx
@@ -31,6 +31,27 @@ describe('testing upload component', (): void => {
     await flushPromises() ;
     expect(setLoading.mock.calls).toHaveLength(2);
   })
+  it('isLoading should be changed to true at first and then to false value',  async (): Promise<void> => {
+    expect.assertions(2);
+    const setImageSource = jest.fn();
+    const setLoading = jest.fn();
+    fetchMock.postOnce('http://localhost:3000/upload-image', {
+      data: {
+        url: 'mockUrl'
+      }
+    });
+    const component = shallow(<UploadComponent setImageSrc={setImageSource} setLoading={setLoading}/>);
+    component.find('input').simulate('change', {
+      target: {
+        files: [
+          'dummy'
+        ]
+      }
+    });
+    await flushPromises() ;
+    expect(setLoading.mock.calls[0][0]).toBe(true);
+    expect(setLoading.mock.calls[1][0]).toBe(false);
+  })
 
   it('setImageSource should be called with url returned from API',  async (): Promise<void> => {
     expect.assertions(1);
