b08f8c2aae51ba00b45c151db0d957e0aeccc849
nawojmonika <nawojmonika@gmail.com>
2020-09-24 20:54:09 +0200

    tested gallery with error message"

--- src/components/gallery-component/gallery-component.spec.tsx
+++ src/components/gallery-component/gallery-component.spec.tsx
@@ -11,6 +11,7 @@ describe('testing gallery component', (): void => {
     fetchMock.restore();
   });
   it('should have gallery with 9 dog photos', async (): Promise<void> => {
+    expect.assertions(1);
     await act(async (): Promise<void> => {
       fetchMock.getOnce("https://dog.ceo/api/breed/husky/images/random/9", {
         message: [
@@ -43,6 +44,19 @@ describe('testing gallery component', (): void => {
       await flushPromises();
       component.update();
       expect(component.find('picture')).toHaveLength(9);
+    })
+  });
+
+  it('should shouw error message when API is down', async (): Promise<void> => {
+    expect.assertions(1);
+    await act(async (): Promise<void> => {
+      fetchMock.getOnce("https://dog.ceo/api/breed/husky/images/random/9",500);
+      fetchMock.getOnce("https://dog.ceo/api/breed/cairn/images/random/9",500);
+      const component = mount(<GalleryComponent breedName={'husky'}/>);
+      component.setProps({breedName: 'cairn'});
+      await flushPromises();
+      component.update();
+      expect(component.exists('.error-component')).toBe(true);
     })
   });
 
